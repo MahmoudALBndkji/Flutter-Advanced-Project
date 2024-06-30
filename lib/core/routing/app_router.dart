8aa0d975449ea844999f8daf3e515055b99b9f12
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2024-06-30 22:45:48 +0300

    Change Login Feature Path

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_project/core/routing/routes.dart';
-import 'package:flutter_advanced_project/features/login/login_screen.dart';
+import 'package:flutter_advanced_project/features/login/ui/login_screen.dart';
 import 'package:flutter_advanced_project/features/onboarding/onboarding_screen.dart';
 
 class AppRouter {
