6f4a1d11e745dfa0f66d22db284a66ce1edc09a1
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2024-06-30 22:25:18 +0300

    Configuration Project

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:flutter_advanced_project/core/routing/app_router.dart';
 import 'package:flutter_advanced_project/doc_app.dart';
 
 void main() {
-runApp(DocApp(appRouter: AppRouter()));
+  runApp(DocApp(appRouter: AppRouter()));
 }
